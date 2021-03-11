ff4f880b085dd2b07f975551db03d03c714535b0
Richard Estes <richard.estes48@gmail.com>
2021-03-10 17:34:34 -0800

    used the wrong list!

--- src/Pages/ProjectsPage.js
+++ src/Pages/ProjectsPage.js
@@ -10,7 +10,7 @@ function ProjectsPage() {
   const [menuItems, setMenuItems] = useState(portfolios);
 
   useEffect(() => {
-    setCategories(portfolios);
+    setCategories(allCategories);
   }, []);
 
   const filter = (category) => {
